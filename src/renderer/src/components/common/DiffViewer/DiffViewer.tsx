c1d995059e8ab39253aec448dbd7399cdd73bdc4
wladimiiir <wladimiiir@gmail.com>
2025-12-04 07:43:32 +0100

    chore: reverted unwanted change

--- src/renderer/src/components/common/DiffViewer/DiffViewer.tsx
+++ src/renderer/src/components/common/DiffViewer/DiffViewer.tsx
@@ -93,12 +93,6 @@ export const DiffViewer = ({ oldValue, newValue, language, isComplete = false }:
       className="diff-viewer"
       optimizeSelection={true}
       tokens={tokens}
-      gutterEvents={{
-        onClick: () => {
-          // eslint-disable-next-line no-console
-          console.log('Gutter clicked');
-        },
-      }}
     >
       {(hunks) => hunks.map((hunk) => <Hunk key={hunk.content} hunk={hunk} />)}
     </Diff>
